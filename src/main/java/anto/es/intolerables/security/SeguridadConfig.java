a8e552500ca45ebaa8f9b0cf55d5d0e5f03333e3
Mariantonieta <Mariantonietachacondibacco@gmail.com>
2025-05-16 23:07:54 +0200

    Añadiendo url para el frontend que ya esta desplegado

--- src/main/java/anto/es/intolerables/security/SeguridadConfig.java
+++ src/main/java/anto/es/intolerables/security/SeguridadConfig.java
@@ -88,7 +88,7 @@
         @Bean
         public CorsConfigurationSource corsConfigurationSource() {
             CorsConfiguration configuration = new CorsConfiguration();
-            configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+            configuration.setAllowedOrigins(List.of("https://intolerables-mariantos-projects.vercel.app/"));
             configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
             configuration.setAllowedHeaders(List.of("*"));
             configuration.setAllowCredentials(true);
